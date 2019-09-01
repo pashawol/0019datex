4725228799090d0803528988d8ab6a3639965646
Roman Makeev <elvizlir@gmail.com>
2019-09-01 21:40:26 +0300

    slider pagination

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -205,6 +205,10 @@ jQuery(document).ready(function ($) {
 		breakpointsInverse: true,
 		speed: 400,
 		loop: true,
+		pagination: {
+			el: $(this).find('.swiper-pagination'),
+			clickable: true,
+		},
 		navigation: {
 			nextEl: ('.s-catalog-slider__slider-next'),
 			prevEl: ('.s-catalog-slider__slider-prev'),

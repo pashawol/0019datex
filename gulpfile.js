2e48421fca1374611d2b14d720e8c8909ce2a5f3
Pavel <wol1414@gmail.com>
2019-10-20 20:07:01 +0300

    add page rent

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,19 @@
 global.$ = {
 	gulp: require('gulp'),  
 	del:  require('del'), 
+	babel:  require('gulp-babel'), 
 	cleanCSS: require('gulp-clean-css'),
 	gulpif: require('gulp-if'),
 	sassGlob: require('gulp-sass-glob'),
+	tabify: require('gulp-tabify'),
 	envDev: false,
 	gp: require('gulp-load-plugins' )(),
 	browserSync: require('browser-sync').create(), 
 	path: {
 		tasks: require('./gulp/config/tasks.js'),
-	}
+	},
+	public: 'public',
+	sourse: 'sourse',
 }
 $.path.tasks.forEach(function (taskPath){
 		require(taskPath)();
@@ -24,6 +28,9 @@ $.gulp.task('img', $.gulp.series('cleanimg','img-responsive'));
 		 
 $.gulp.task('default', $.gulp.series(
 	// $.gulp.parallel('svg','pug','scripts:lib','scripts','file'),
-	$.gulp.parallel('svg','pug','img','scripts','file'),
+	// $.gulp.parallel('file'),
+	$.gulp.parallel(
+		'svg','pug','img','scripts', 
+		'scripts:common'),
 	$.gulp.parallel('sass','watch','serv')
 	));
